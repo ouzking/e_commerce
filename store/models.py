54c6164d16ed4b39347ea293cfc989a91ec88ba2
ouzking <ousmanearfangkemobodian@gmail.com>
2023-06-20 16:52:19 +0200

    first commit

---
+++ store/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.urls import reverse
+from commerce.settings import AUTH_USER_MODEL
+from django.utils import timezone
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    price = models.FloatField(default=0.0)
+    stock = models.IntegerField(default=0)
+    description = models.TextField(blank=True)
+    thumbnail = models.ImageField(upload_to="products", blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+    
+    
+    def get_absolute_url(self):
+        return reverse("product", kwargs={"slug": self.slug})
+    
+    
+class Order(models.Model):
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    ordered = models.BooleanField(default=False)
+    orderd_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.product.name} ({self.quantity})" 
+    
+class Cart(models.Model):
+    user = models.OneToOneField(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    orders = models.ManyToManyField(Order)
+    
+    def __str__(self):
+        return self.user.username
+    
+    def delete(self, *args, **kwargs):
+        for order in self.orders.all():
+            order.ordered = True
+            order.ordered_date = timezone.now()
+            order.save()
+            
+        self.orders.clear()
+        super().delete(*args, **kwargs)
